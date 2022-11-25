bde331c2b53df3f461904c33c8128464c0150462
Narumi_Mashiro <narunarumashiro96@gmail.com>
2022-11-25 23:06:34 +0900

    split repository

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <nav-bar />
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import NavBar from '@/components/layouts/NavBar.vue';
+
+export default {
+  components: {
+    NavBar,
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 0 2rem;
+  text-align: center;
+  color: #502c2c;
+}
+
+body {
+  background-color: #fdeeef;
+}
+</style>
