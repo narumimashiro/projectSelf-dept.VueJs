ae7cce22fe1a6a1370a9648535f9538eb7b0dc88
Narumi_Mashiro <narunarumashiro96@gmail.com>
2022-11-27 15:14:14 +0900

    miku footerの実装

--- src/components/main/NewsView.vue
+++ src/components/main/NewsView.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="news">
-    <h1>This is a news page</h1>
+  <div>
+    <h1>This is a News page</h1>
+    <footer-miku />
   </div>
 </template>
+
+<script lang="ts">
+import FooterMiku from '@/components/layouts/FooterMiku.vue'
+
+export default {
+  data: () => {
+    return {
+
+    }
+  },
+  components: {
+    FooterMiku,
+  }
+}
+</script>
